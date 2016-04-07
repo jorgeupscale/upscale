165bf0691fd24ef14005b6a8d945f4b93b21e8f8
barksntales <barkstales@hotmail.com>
2016-04-06 22:39:27 -0400

    translation

--- public/contact_me.php
+++ public/contact_me.php
@@ -1,8 +1,8 @@
 <?php
 if($_POST)
 {
-    $to_Email       = "myemail@gmail.com"; //Replace with recipient email address
-    $subject        = 'Ah!! My email from Somebody out there...'; //Subject line for emails
+    $to_Email       = "info@upscalecarrental.com"; //Replace with recipient email address
+    $subject        = 'Upscale Car Rental '; //Subject line for emails
     
     
     //check if its an ajax request, exit if not
